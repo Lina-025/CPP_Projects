80e011a3502d31cb045249d2333ba63e86a1fd04
Lina Boukrouh <linaboukrouh3@gmail.com>
2025-06-15 22:18:40 +0300

    nm

---
+++ ternary_operator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+//int grade = 50;
+
+//grade >= 60? cout << "you pass!" : cout << "you fail!";
+
+bool hungry = false; // we set to true or false
+//hungry ? cout << "you're hungry" : cout << "you're full";
+cout << (hungry? "youre hungry": "youre full"); //this also works  
+return 0;
+
+}
