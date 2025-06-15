8a347dfdf3dc10648379c5a7cbd152a431b7fe19
Lina Boukrouh <linaboukrouh3@gmail.com>
2025-06-16 01:26:03 +0300

    nm

---
+++ functions.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+void happybirthday(){
+   string name = "lina";
+    cout << "Happy birthday to " << name << '\n';
+    cout << "Happy birthday to " << name << '\n';
+    cout << "Happy birthday dear " << name << '\n';
+    cout << '\n'; 
+
+}
+int main(){
+ 
+    happybirthday();
+    happybirthday();
+    happybirthday();
+    return 0;
+}
