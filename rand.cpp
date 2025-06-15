97d94739d2ddb3aba0f2f87f2df1a51917f24983
Lina Boukrouh <linaboukrouh3@gmail.com>
2025-06-16 00:33:20 +0300

    nm

--- rand.cpp
+++ rand.cpp
@@ -6,12 +6,22 @@ int main(){
 
     srand(time(NULL));
 
-    int num1 = (rand() % 6) +1;
-    int num2 = (rand() % 6) +1;
-    int num3 = (rand() % 6) +1;
-    cout << num1 << '\n';
-    cout << num2 << '\n';
-    cout << num3 << '\n';
+    int num1 = (rand() % 5) +1;
+    
+    switch(num1) {
+        case 1: cout << "you win a chocolate bar!\n";
+        break;
+        case 2:cout << "you win a jacket!\n";
+        break;
+        case 3:cout << "you win a free dinner!\n";
+        break;
+        case 4:cout << "you win a gift card!\n";
+        break;
+        case 5:cout << "you win opera tickets!!\n";
+        break;
+    }
+    
+    
 
 
     return 0;
