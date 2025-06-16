31193107416a3ca58b2311ddbc40317fa44ebada
Lina Boukrouh <linaboukrouh3@gmail.com>
2025-06-16 03:17:55 +0300

    nm

---
+++ sizeof.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    double gpa = 2.5;
+    string name = "lina";
+    char grade = 'B';
+    char grades[] = {'A', 'B', 'C', 'D', 'F'};
+    cout << sizeof(grades)/sizeof(grades[0]) << " elements\n";
+    
+
+
+    return 0;
+}
+    
