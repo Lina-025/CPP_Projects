b4942fd3078c5c110b7e13c34db5cc9c003c2025
Lina Boukrouh <linaboukrouh3@gmail.com>
2025-06-16 03:11:13 +0300

    nm

---
+++ arrays.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    /*string car[3] = {"Batomobile", "mustang", "toyota"};
+    car[0] = "Ferrari"; //re-assigning the first element
+    cout << car [2]<< endl;*/
+
+    double prices[]= {5.00, 7.50,  9.99, 15.00};
+
+    cout << prices[0] << endl; 
+}
