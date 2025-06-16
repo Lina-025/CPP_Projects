3e9c92b564735aad7056c76cefa857b6d58eaa5a
Lina Boukrouh <linaboukrouh3@gmail.com>
2025-06-16 03:29:01 +0300

    nm

---
+++ foreachloop.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string students[] = {"lina", "sara", "hiba", "manel"};
+
+    for (string student : students){
+        cout << student << endl;
+    }
+} 
